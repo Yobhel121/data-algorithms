3ee16e86c0edea95debfbf5a4c80debdf77da462
yezhimin <976855420@qq.com>
2019-10-31 18:00:19 +0800

    K-mer计数

---
+++ data-algorithms-mr/src/main/java/org/dataalgorithms/chap17/mapreduce/KmerCountReducer.java
@@ -0,0 +1,29 @@
+package org.dataalgorithms.chap17.mapreduce;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+//
+
+/**
+ * A reducer class that just emits the sum of the input values.
+ *
+ * @author Mahmoud Parsian
+ *
+ */
+public class KmerCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+
+}
